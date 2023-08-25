e00ea89d8811befcbfec80ff9ffa317fb74df738
Logan Prust <ljprust@kitp.ucsb.edu>
2023-08-25 15:36:37 -0500

    updated params

--- Initial/sn.c
+++ Initial/sn.c
@@ -8,10 +8,10 @@ void initial( double * prim , double r ){
 
    double rho, P, v, X;
 
-   double Eej = 1.0e51;
-   double Mej = 2.0e33;
-   double t0  = 7.8894e8; // 25 yr
-   double rhoISM = 1.6e-24;
+   double Eej = 1.3e51; // 1.0e51;
+   double Mej = 1.4*2.0e33; // 2.0e33;
+   double t0  = 3.15576e8; // 10 yr // 7.8894e8; // 25 yr
+   double rhoISM = 5.0e-25; // 1.6e-24;
    double vmax = 2.0e9;
 
    double v0 = sqrt(4.0/3.0*Eej/Mej);
